092379f9bda04955e55b5120d664ace21cadee80
linkin <89432137+sinkinlimpson@users.noreply.github.com>
2026-08-29 13:50:20 -0400

    Implemented bullet cooldown, updated inputs

--- weapon.gd
+++ weapon.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
 @export var bulletScene: PackedScene
+var cooldown = 0.2
 
 func _process(delta):
 	look_at(get_global_mouse_position())
 
-	if Input.is_action_just_pressed("mouse_click"):
-		shoot()
+	if Input.is_action_pressed("mouse_click"):
+		if cooldown <= 0:
+			shoot()
+			cooldown = 0.2
+	cooldown -= delta
 
 func shoot():
 	var bullet = bulletScene.instantiate()
