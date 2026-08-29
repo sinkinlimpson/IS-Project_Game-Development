0ffee589ff5364a0f6e296423d355896223a4ac6
linkin <89432137+sinkinlimpson@users.noreply.github.com>
2026-08-29 13:37:31 -0400

    Added basic bullet and shooting functionality

--- weapon.gd
+++ weapon.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
-
-func _ready():
-	pass
+@export var bulletScene: PackedScene
 
 func _process(delta):
 	look_at(get_global_mouse_position())
+
+	if Input.is_action_just_pressed("mouse_click"):
+		shoot()
+
+func shoot():
+	var bullet = bulletScene.instantiate()
+	bullet.position = $Muzzle.global_position
+	bullet.direction = (get_global_mouse_position() - global_position).normalized()
+	get_tree().root.add_child(bullet)
