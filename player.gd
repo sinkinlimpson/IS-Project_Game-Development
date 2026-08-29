53fb013697bfe8172f952e76bf3ce05837133ceb
linkin <89432137+sinkinlimpson@users.noreply.github.com>
2026-08-29 10:52:35 -0400

    Add player movement

--- player.gd
+++ player.gd
@@ -8,4 +8,16 @@ func _physics_process(delta):
 	#handle player input
 
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	print(direction)
+
+	# handle movement
+
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+		velocity = direction * SPEED
+	else:
+		velocity = Vector2.ZERO
+
+	print(velocity)
+
+	move_and_slide()
+	
