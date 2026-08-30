6629f8034186cddd53091c0d06f23f1490c71d4d
linkin <89432137+sinkinlimpson@users.noreply.github.com>
2026-08-30 12:15:40 -0400

    Implemented bullet collision with enemy

--- bullet.gd
+++ bullet.gd
@@ -1,7 +1,12 @@
-extends Node2D
+extends Area2D
 
 @export var speed: float = 500.0
 @export var direction: Vector2 = Vector2.RIGHT
 
 func _process(delta):
 	position += direction * speed * delta
+
+func _on_body_entered(body: Node2D):
+	if body.is_in_group("enemies"):
+		body.takeDamage(1)
+		queue_free()
--- enemy.gd
+++ enemy.gd
@@ -1,6 +1,14 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @export var health = 5
 
 func _process(delta):
 	pass
+
+func takeDamage(amount):
+	health -= amount
+	if health <= 0:
+		die()
+
+func die():
+	queue_free()
