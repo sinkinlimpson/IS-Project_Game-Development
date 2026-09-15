f1426f5d6488e89878decf0d491cd617941b0008
linkin <89432137+sinkinlimpson@users.noreply.github.com>
2026-09-15 10:02:11 -0400

    Began to revamp player functionality
    Removal of gun, altered sprites, began implementing state machine

--- player.gd
+++ player.gd
@@ -7,6 +7,10 @@ const FRICTION = 900.0
 
 @export var health = 5
 
+enum States {IDLE, WALKING, ATTACKING, ROLLING, DEAD}
+
+var state: States = States.IDLE
+
 func _physics_process(delta):
 
 	#handle player input
@@ -17,10 +21,28 @@ func _physics_process(delta):
 
 	if direction != Vector2.ZERO:
 		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+		state = States.WALKING
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
+		state = States.IDLE
+
 	move_and_slide()
+	handleAnim(direction)
+
+func handleAnim(direction):
+	match state:
+		States.WALKING:
+			if abs(direction.x) > abs(direction.y):
+				if direction.x > 0:
+					$AnimationPlayer.play("right")
+				else:
+					$AnimationPlayer.play("left")
+			elif abs(direction.y) > abs(direction.x):
+				if direction.y > 0:
+					$AnimationPlayer.play("down")
+				else:
+					$AnimationPlayer.play("up")
 
 func takeDamage(amount):
 	health -= amount
